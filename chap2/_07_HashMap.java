4fdc8c3c95fce4f772972aef647603c314042d3d
tagi23 <57087080+tagi23@users.noreply.github.com>
2023-06-02 19:04:54 +0900

    HashMap

---
+++ chap2/_07_HashMap.java
@@ -0,0 +1,79 @@
+package chap_5;
+
+import java.util.HashMap;
+
+public class _07_HashMap {
+	public static void main(String[] args) {
+		//map   (key , Value)
+		HashMap<String , Integer> map = new HashMap<>();
+		
+		//데이터 추가 
+		map.put("유재석", 10);
+		map.put("박명수", 5);
+		map.put("김종국", 3);
+		map.put("서장훈", 15);
+		
+		//map 수
+		System.out.println("총 고객 수 "+ map.size());
+		System.out.println("---------------------------------");
+		
+		//조회
+		System.out.println("유재석님의 포인트 : " + map.get("유재석"));
+		System.out.println("박명수님의 포인트 : " + map.get("박명수"));
+		System.out.println("김종국님의 포인트 : " + map.get("김종국"));
+		System.out.println("---------------------------------");
+		
+		//확인
+		if (map.containsKey("서장훈")) {
+			int point = map.get("서장훈");
+			map.put("서장훈",++point);
+			System.out.println("서장훈님의 포인트 : " + map.get("서장훈"));
+		} else {
+			map.put("서장훈",1);
+			System.out.println("서장훈님의 신규 등록 포인트 : " + map.get("서장훈"));
+		}
+		System.out.println("---------------------------------");
+		// 삭제 
+		map.remove("유재석");
+		System.out.println(map.get("유재석"));
+		System.out.println("---------------------------------");
+		
+		// 전체 삭제 
+		map.clear();
+		if(map.isEmpty()) {
+			System.out.println("남은 고객 수 : "+ map.size());
+		}
+		System.out.println("---------------------------------");
+		
+		map.put("유재석", 10);
+		map.put("박명수", 5);
+		map.put("김종국", 3);
+		map.put("서장훈", 15);
+		System.out.println("---------------------------------");
+		// key 확인   순서 보장 x
+		for (String s :map.keySet()) {    // 키 값을 받아옴
+			System.out.println(s);
+		}
+		
+		System.out.println("---------------------------------");
+		//value 확인
+		for (int  i :map.values()) { // 밸류  값을 받아옴
+			System.out.println(i);
+		}
+		
+		System.out.println("---------------------------------");
+		// key & value 함께 확인
+		for (String key:  map.keySet()) {
+			System.out.println("고객 이름 : " + key + " \t포인트 : " + map.get(key) );
+		}
+		System.out.println("---------------------------------");
+		map.put("김종국", 3);
+		map.put("김종국", 5);
+		map.put("김종국", 17);
+		for (String key:  map.keySet()) {
+			System.out.println("고객 이름 : " + key + " \t포인트 : " + map.get(key) );
+		}
+		// LinkedHashmap 을 쓰면은 순서보장 o 
+		
+	}
+}
