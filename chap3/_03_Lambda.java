0043fda620775bb70f5eea1713e9be5c5a34241d
tagi23 <57087080+tagi23@users.noreply.github.com>
2023-06-03 20:20:13 +0900

    람다식

---
+++ chap3/_03_Lambda.java
@@ -0,0 +1,28 @@
+package chap_6;
+
+public class _03_Lambda {
+
+	public static void main(String[] args) {
+		// 람다식 (간결한 형태의 코드 모음)      >>접근제어자가 필요없음 , 자동 리턴
+		// (전달값1 , 전달값2 , ....) -> {코드}     
+	}
+	
+//	public void print() {
+//		String s = "test";
+//		System.out.println(s);
+//	}
+	
+//	() -> {
+//		String s = "test";
+//		System.out.println(s);
+//	}
+	
+//	s -> System.out.println(s)
+	
+//	public int add (int x , int y) {
+//		return x+y;
+//	}
+	
+//	(x , y) ->  x+y     //리턴이 포함시 중괄호는 포함
+	
+}
