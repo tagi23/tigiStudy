05c1a1d034c451523bbaaf7a3fca496168904971
tagi23 <57087080+tagi23@users.noreply.github.com>
2023-06-07 23:06:58 +0900

    파일 삭제

---
+++ chap6/_07_DeleteFile.java
@@ -0,0 +1,52 @@
+package chap_9;
+
+import java.io.File;
+
+public class _07_DeleteFile {
+
+	public static void main(String[] args) {
+		File file = new File ("goodjob.txt");  //파일 삭제
+		
+		if(file.exists()) {
+			if(file.delete()) {
+				System.out.println("파일 삭제 성공 : " + file.getName());
+			}else {
+				System.out.println("파일 삭제 실패 : " + file.getName());
+			}
+		}
+		
+		File folder = new File("A"); //폴더 삭제
+		if(folder.exists()) {
+			if(folder.delete()) {
+				System.out.println("폴더 삭제 성공 : " + folder.getAbsoluteFile());
+			}else {
+				System.out.println("폴더 삭제 실패 : "+ folder.getAbsolutePath());
+			}
+		}		
+		System.out.println("----------------------------");
+		
+		if(deleteFolder(folder)) {
+			System.out.println("*폴더 삭제 성공  : " + folder.getAbsolutePath());
+		}else {
+			System.out.println("*폴더 삭제 실패 : "  + folder.getAbsolutePath());
+		}
+		
+	}
+	
+	//deleteFolder(A)
+	//   				deleteFolder(B)
+	//								deleteFolder(C)
+	//								B.delete() 삭제
+	//					B.delete() 삭제
+	//A.delete() 삭제;
+	public static boolean deleteFolder (File folder) {   //한꺼번에 삭제하기 (재귀함수)
+		if (folder.isDirectory()) {
+			for(File file : folder.listFiles()) {
+				deleteFolder(file);
+			}
+		}
+		System.out.println("삭제 대상 : " + folder.getAbsolutePath());
+		return folder.delete();
+	}
+
+}
