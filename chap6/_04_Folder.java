fd76e7eab5d1479c9c037ef6dbd5790e94d827ae
tagi23 <57087080+tagi23@users.noreply.github.com>
2023-06-07 23:05:51 +0900

    폴더 다루기

---
+++ chap6/_04_Folder.java
@@ -0,0 +1,29 @@
+package chap_9;
+
+import java.io.File;
+
+public class _04_Folder {
+
+	public static void main(String[] args) {
+		String folderName = "A";
+		File folder = new File(folderName);
+		folder.mkdir();   // 폴더 만들기
+		if(folder.exists()) {
+			System.out.println("폴더가 존재합니다 : " + folder.getAbsolutePath());
+		}
+		System.out.println("-----------------------------");
+		folderName = "A\\B\\C";
+		// folderName = "A" + File.separator + "B"+ File.separator + "C";
+		// 윈도우 : C:\\Users\\KIM\\...
+		// 맥 : \Users\KIM\...
+		folder = new File(folderName);
+		// folder.mkdir();   //단일 폴더 만들기
+		folder.mkdirs();   //여러 폴더 만들기
+		if(folder.exists()) {
+			System.out.println("폴더가 존재합니다 : " + folder.getAbsolutePath());
+		}else {
+			System.out.println("폴더 생성 실패");
+		}
+	}
+
+}
